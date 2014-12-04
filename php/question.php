0682a93e762b36d71cecc1f578d8952d34cb718c
hubologist <hubologist@gmx.com>
2014-12-04 15:51:00 +0000

    Added pastel color generator Figured out how to create 4 separate buttons with the intended layout Still need to fix missing values: flags and capitals Also, random number generator is not perfect and can generate a number outside the intended range

--- php/question.php
+++ php/question.php
@@ -5,3 +5,23 @@
  * contact: hi@lightradius.com
  */
 
+
+
+function getQuestion() {
+    $rand = mt_rand(0, 3);
+    switch ($rand) {
+        case 0:
+            $question = "What is the capital of ";
+            break;
+        case 1:
+            $question = "Which country does this flag belong to?";
+            break;
+        case 2:
+            $question = "Which of these has the largest area?";
+            break;
+        case 3:
+            $question = "Which of these has the largest population?";
+            break;
+    }
+    return $question;
+}
