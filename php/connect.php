769750e97b356681f9060865b2e1feaf12993b63
hubologist <hubologist@gmx.com>
2014-12-07 13:32:20 +0000

    Restructuring everything - making progress 3 New flags, standardized ratio 3:2
    Still need to fix missing values: flags and capitals
    Also, random number generator is not perfect and can generate a number outside the intended range

--- php/connect.php
+++ php/connect.php
@@ -10,10 +10,10 @@ $username = "root";
 $password = "";
 $dbname = "geomancer";
 
-// Create connection
+//Create connection
 $db = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+//Check connection
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error . "<br />");
 }
@@ -21,9 +21,9 @@ if ($db->connect_error) {
 //The only query we'll ever need
 $sql = "SELECT iso, name, capital, area, pop, continent, neighbours FROM countries WHERE capital IS NOT NULL";
 $result = $db->query($sql);
-//For each randomly selected id, we will select the corresponding row and store it in a multidimensional array
+
 if ($result->num_rows > 0) {
-    // Save data of each row
+    //Save data of each row
     while ($row = $result->fetch_assoc()) {
         $country[] = $row;
     }
